8e01f0b344e575661c85565b250e4aedc10e63e2
valentina abramova <valentina@v.local>
2017-11-03 16:10:45 -0500

    more random s

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,26 @@
 import React,  { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+import './button.css';
 import { connect } from 'react-redux';
 
 export default class Button extends Component {
-    render() {
-      
-  
-      return (<div>
-          Button
-          </div>
-      )}
+    constructor(props) { 
+        super(props) 
+        this.state = {
+            disabled: false
+        }
     }
+
+    render() {
+        return (
+                <button 
+                    className={`button button-${this.props.type}`} 
+                    disabled={this.state.disabled} 
+                    onClick={this.props.action} 
+                    style={this.props.style} 
+                >
+                    {this.props.text}
+                </button>
+        )
+    }
+}
