78be01210cc06d55919243c2945b5a12dfe17752
valentina abramova <valentina@v.local>
2017-11-03 13:33:22 -0500

    HOPE THIS WORKS

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React,  { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import { connect } from 'react-redux';
+
+export default class Header extends Component {
+    render() {
+      
+  
+      return ( <div>
+         Header
+          </div>
+   ) }}
