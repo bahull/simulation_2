f594221e238d2c397203b465e3b511b3cc60422a
ccorkery97 <ccorkery97@gmail.com>
2017-11-03 12:13:16 -0500

    commit4

---
+++ db/Create_Property.sql
@@ -0,0 +1,2 @@
+INSERT INTO Properties (name,descrition,address,city,state,zip,image,loan_amount,monthly_mortgage,desired_rent)
+values (name,description,address,city,state,zip,image,loan_amount,monthly_mortgage,desired_rent);
